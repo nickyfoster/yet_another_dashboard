aead4a5733ad3b06f78c6cf7d20cb6fe1829505a
Nikita Gulyaev <n.gulyaev@just-ai.com>
2022-11-09 16:24:59 -0300

    ECParser updated

--- dash_core/services/ECParser.py
+++ dash_core/services/ECParser.py
@@ -1,7 +1,6 @@
+import bs4
 import requests
-from bs4 import BeautifulSoup
 import unidecode
-from requests.exceptions import MissingSchema
 
 from dash_core.services.Exception import CustomException
 from dash_core.services.ExceptionCode import ExceptionCode
@@ -9,30 +8,51 @@ from dash_core.services.ExceptionMessage import ExceptionMessage
 
 
 class ECParser:
+    """
+    Econ Calendar parsing logic.
+    """
     def __init__(self, url=None):
         self.url = url if url else "https://ibd.econoday.com/byweek.asp?cust=ibd&cty=US&lid=0"
+        self.calendar_data = dict()
         self.html_text = self.get_html_text(self.url)
         if self.html_text:
-            self.soup = BeautifulSoup('html.parser')
+            self.soup = bs4.BeautifulSoup(markup=self.html_text, features='html.parser')
 
-    def parse_calendar(self):
+    def get_econ_calendar(self) -> dict:
+        """Economic Calendar parser. We get main table from HTML source and split into two parts:
+        1. Days. Here we get weekdays and their numbers.
+        2. Events. Here we get economic events with time and description.
+        :return: dict, calendar_data
+        """
         events = self.soup.find_all("table", {"class": "eventstable"})
-        calendar_data = {}
         if len(events) == 1:
-            table_days_names = events[0].find_all_next("td", {"class": ["navwkday", "currentnavwkday"]})
-            for table_days_name in table_days_names:
-                calendar_data[unidecode.unidecode(table_days_name.text)] = None
-                print(table_days_name.text)
+            self._get_calendar_column_names(events)
+            self._parse_calendar_column_data(events)
+        else:
+            raise CustomException(code=ExceptionCode.INTERNAL_SERVER_ERROR,
+                                  message=ExceptionMessage.DASHCORE_URL_UNABLE_TO_PARSE_HTML)
+
+    def _get_calendar_column_names(self, events: bs4.element.ResultSet):
+        table_data_days = events[0].find_all_next("td", {"class": ["navwkday", "currentnavwkday"]})
+        for table_data_day in table_data_days:
+            self.calendar_data[unidecode.unidecode(table_data_day.text)] = None
+        if len(self.calendar_data) != 5:
+            raise CustomException(code=ExceptionCode.INTERNAL_SERVER_ERROR,
+                                  message=ExceptionMessage.DASHCORE_URL_UNABLE_TO_PARSE_WEEK)
+
+    def _parse_calendar_column_data(self, events: bs4.element.ResultSet):
+        table_data_events = events
+        print(events)
 
     @staticmethod
     def get_html_text(url):
         try:
-            res = requests.get(url).text
-            return res
-        except MissingSchema:
+            result = requests.get(url).text
+        except Exception:
             raise CustomException(code=ExceptionCode.BAD_REQUEST,
                                   message=ExceptionMessage.DASHCORE_URL_INVALID)
+        return result
 
 
-parser = ECParser(url="ass")
-print(parser.url)
+parser = ECParser()
+parser.get_econ_calendar()
--- dash_core/services/ExceptionMessage.py
+++ dash_core/services/ExceptionMessage.py
@@ -4,3 +4,5 @@ from enum import Enum
 class ExceptionMessage(Enum):
     DASHCORE_URL_INVALID = 'dashcore.url.invalid'
     DASHCORE_URL_NOT_FOUND = 'dashcore.url.not_found'
+    DASHCORE_URL_UNABLE_TO_PARSE_HTML = 'dashcore.url.unable_to_parse_html_table'
+    DASHCORE_URL_UNABLE_TO_PARSE_WEEK = 'dashcore.url.unable_to_parse_week_columns'
